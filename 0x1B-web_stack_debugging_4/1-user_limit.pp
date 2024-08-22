9f127414450069a103f9c3cef591a6a70fc3d24b
Tsohle Mokhemisi <mokhemisitsohle@gmail.com>
2024-08-22 11:29:31 +0200

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,33 @@
-#Enable the user holberton to login and open files without errors
+# 1-user_limit.pp
+# Allowing higher hard/soft file open limits
 
-# Increase hard file limit for holberton user
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+$file = '/etc/security/limits.conf'
+
+$hard_txt = 'holberton hard nofile'
+$hard_rep = "${::hard_txt} 10000"
+$soft_txt = 'holberton soft nofile'
+$soft_rep = "${::soft_txt} 8000"
+
+$hard_exists = "grep ${::hard_rep} ${::file}"
+$soft_exists = "grep ${::soft_rep} ${::file}"
+
+$hard_cmd = "sed -i '/${::hard_txt} /c ${::hard_rep}' ${::file}"
+$soft_cmd = "sed -i '/${::soft_txt} /c ${::soft_rep}' ${::file}"
+
+exec { 'hard_limit_fix':
+  path    => '/bin',
+  unless  => $hard_exists,
+  command => $hard_cmd,
+}
+exec { 'soft_limit_fix':
+  path    => '/bin',
+  unless  => $soft_exists,
+  command => $soft_cmd,
 }
 
-#Increase soft file limit for user holberton
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+exec { 'notify_for_relogin':
+  command     => 'echo "Please logout and login again or restart the system to apply file descriptor limits."',
+  path        => '/bin',
+  subscribe   => [Exec['soft_limit_fix'], Exec['hard_limit_fix']],
+  refreshonly => true,
 }
