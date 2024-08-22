f91d97dae343f6b12e75462b71a96d8293ad3c38
Tsohle Mokhemisi <mokhemisitsohle@gmail.com>
2024-08-22 12:46:43 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,22 +5,26 @@ Get number of users from Reddit
 
 import requests
 
-def number_of_subscribers(subreddit):
-    """Returns the number of subscribers to a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers if the subreddit is valid, otherwise 0.
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'user-agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-         return 0
-    data = response.json()
-    if not data:
-         return 0
-    return data['data']['subscribers']
+def get_subreddit_subscribers(subreddit):
+    # Define the URL for the subreddit
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    
+    # Set the User-Agent to avoid request being blocked by Reddit
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    
+    try:
+        # Make the request to Reddit API
+        response = requests.get(url, headers=headers)
         
+        # Check if the response status code is 200 (OK)
+        if response.status_code == 200:
+            data = response.json()
+            # Extract the number of subscribers
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            # If the subreddit is invalid, return 0
+            return 0
+    except Exception as e:
+        # In case of any other error, return 0
+        return 0
