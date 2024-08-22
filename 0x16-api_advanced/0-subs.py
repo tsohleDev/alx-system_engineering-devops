a1273b2fb9d834705780360a0947c076a2925d19
Tsohle Mokhemisi <mokhemisitsohle@gmail.com>
2024-08-22 13:00:37 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,32 +6,22 @@ Get number of users from Reddit
 import requests
 
 def number_of_subscribers(subreddit):
-    # Define the URL for the subreddit
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    # Set the User-Agent to avoid request being blocked by Reddit
-    headers = {
-        'User-Agent': 'YourAppName/1.0 (by u/YourRedditUsername)',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Connection': 'keep-alive',
-        'DNT': '1',  # Do Not Track request header
-        'Upgrade-Insecure-Requests': '1',
-    }
-    
-    try:
-        # Make the request to Reddit API
-        response = requests.get(url, headers=headers)
-        
-        # Check if the response status code is 200 (OK)
-        if response.status_code == 200:
-            data = response.json()
-            # Extract the number of subscribers
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
-            # If the subreddit is invalid, return 0
-            return 0
-    except Exception as e:
-        # In case of any other error, return 0
+    """Gets number of subscribers
+       Args:
+           subreddit (str): name of subreddit
+       Returns:
+           number of subscribers if valid, 0 otherwise
+    """
+    base_url = 'https://api.reddit.com/r/'
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    response = requests.get(
+        '{}{}/about'.format(
+            base_url, subreddit), headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         return 0
+
+    about_dict = response.json()
+
+    return about_dict['data']['subscribers']
+    
